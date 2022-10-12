82fa5e4f370d0fcc136c12ab1d0b832da4321540
Anton Komarov <anton.komarov116@gmail.com>
2022-10-12 20:04:18 +0300

    Логические операции

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,22 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+
+        bool CPUTemperature = true;
+        bool CPUFan = true;
+
+        if (CPUTemperature || CPUFan)
+        {
+            Console.WriteLine("Everithing okay");
+        }
+        else
+        {
+            Console.WriteLine("CPU temperature critical!");
+        }
+    }
+}
+ 
